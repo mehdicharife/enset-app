50172865f708ec57c30d8b585101b351bf03b404
mehdicharife <mehdicharife@yahoo.com>
2023-11-23 20:59:27 +0100

    generated new product component

---
+++ src/app/new-product/new-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-product.component.html',
+  styleUrl: './new-product.component.css'
+})
+export class NewProductComponent {
+
+}
