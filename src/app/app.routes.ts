a5652d0a2791d97339776cf42abb722b2f6dfeae
mehdicharife <mehdicharife@yahoo.com>
2023-11-23 21:59:00 +0100

    routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Route, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {NewProductComponent} from "./new-product/new-product.component";
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {path: 'home', loadComponent: () => import("./home/home.component").then(mod => mod.HomeComponent)},
+  {path: 'products', loadComponent: () => import("./products/products.component").then(mod => mod.ProductsComponent)},
+  {path: 'new-product', loadComponent: () => import("./new-product/new-product.component").then(mod => mod.NewProductComponent)}
+];
