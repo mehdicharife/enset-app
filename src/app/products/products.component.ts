159f24742213fe1af7b5875b7d74e7553af391d5
mehdicharife <mehdicharife@yahoo.com>
2023-11-24 13:41:04 +0100

    backend fetched products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-  products : Array<any> = [
-    {id: 1, name: "Computer", price: 5620, checked: false},
-    {id: 2, name: "Printer", price: 2616, checked: false},
-    {id: 3, name: "Camera", price: 6800, checked: false}
-  ];
+export class ProductsComponent implements OnInit{
+  products : Array<any> = [];
+  private httpClient : HttpClient;
+
+  constructor(httpClient : HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  ngOnInit() {
+    this.httpClient.get<Array<any>>("http://localhost:8089/products").subscribe({
+      next : value => this.products = value,
+      error : err => console.log(err)
+    });
+  }
+
+  onProductCheck(product : any) {
+    this.httpClient.patch<any>(
+      `http://localhost:8089/products/${product.id}`,
+      {checked : !product.checked}
+    ).subscribe({
+      next : value => this.products.filter(product => product.id == value.id).map(element => element.checked = value.checked),
+      error : err => console.log(err)
+    });
+  }
 }
