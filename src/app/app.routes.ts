30d11b31845172c93c91148ffe1bcdd85f6de534
mehdicharife <mehdicharife@yahoo.com>
2023-11-26 12:27:00 +0100

    login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,6 @@ export const routes: Route[] = [
   {path: 'home', loadComponent: () => import("./components/home/home.component").then(mod => mod.HomeComponent)},
   {path: 'products', loadComponent: () => import("./components/products/products.component").then(mod => mod.ProductsComponent)},
   {path: 'new-product', loadComponent: () => import("./components/new-product/new-product.component").then(mod => mod.NewProductComponent)},
-  {path: 'product-edit/:id', loadComponent: () => import("./components/product-edit/product-edit.component").then(mod => mod.ProductEditComponent)}
+  {path: 'product-edit/:id', loadComponent: () => import("./components/product-edit/product-edit.component").then(mod => mod.ProductEditComponent)},
+  {path: 'login', loadComponent: () => import("./components/login/login.component").then(mod => mod.LoginComponent)}
 ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+  public loginForm! : FormGroup;
+  private formBuilder : FormBuilder;
+
+  constructor(formBuilder : FormBuilder) {
+    this.formBuilder = formBuilder;
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username : this.formBuilder.control("", Validators.required),
+      password : this.formBuilder.control("", Validators.required)
+    });
+  }
+
+  login() {
+
+  }
+
+}
